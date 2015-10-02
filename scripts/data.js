f177dab365da76f60aa0502060b7f17dcfc7f50d
Robotnik19 <justinthomas123456789@yahoo.com>
2015-10-02 13:37:52 -0400

    code louisville code

---
+++ scripts/data.js
@@ -0,0 +1,272 @@
+var cards = [
+	{
+		url: "http://anilist.co/img/dir/anime/reg/20997-8uqxQpmR1laU.jpg",
+		name: "Charlotte",
+		genre: "Drama, School, Supernatural",
+		episodes: "12",
+		duration: "24 min",
+		source: "Original Series",
+		text: "The story centers around the special abilities that occur among a small percentage of boys and girls in puberty. Yuu Otosaka uses his power without others knowing, and lives a fairly normal, average school life. Before him suddenly appears a girl, Nao Tomori. Due to his meeting with her, the fate of special power-users will be exposed."
+	},
+	{
+		url: "http://anilist.co/img/dir/anime/reg/20923-pAB59gMZom6X.jpg",
+		name: "Shokugeki no Soma",
+		genre: "Comedy, Ecchi, School, Shounen",
+		episodes: "24",
+		duration: "25 min",
+		source: "Manga",
+		text: "The anime adaptation of the Shokugeki no Souma manga that centers on Souma, who works with his dad at his family's restaurant. His dad is a culinary master, and he often hosts cooking battles with his father. His father then enrolls him in an elite culinary school that is very difficult to enter, and the graduation rate is only about 10 percent."
+	},
+	{
+		url: "http://anilist.co/img/dir/anime/reg/20807-hkwrTnDAqDiY.jpg",
+		name: "Prison School",
+		genre: "Comedy, Ecchi, School, Seinen",
+		episodes: "12",
+		duration: "24 min",
+		source: "Manga",
+		text: "Hachimitsu Private Academy was a revered and elite all-girls' boarding school on the outskirts of Tokyo...once upon a time. But with the new school year comes a revision to school policy: Boys are to be admitted into the student body for the first time ever. But on his first day at Hachimitsu, Kiyoshi Fujino discovers that he's one of only five boys enrolled at the school. Their numbers overwhelmed by the thousand girls in the student body, is it heaven or hell that awaits these five (un)fortunates?!."
+	},
+	{
+		url: "http://anilist.co/img/dir/anime/reg/20920-W0MQotcMHHpv.jpg",
+		name: "Dungeon ni Deai o Motomeru no wa Machigatteiru Darou ka",
+		genre: "Action, Adventure, Fantasy, Comedy, Romance",
+		episodes: "13",
+		duration: "24 min",
+		source: "Light Novel",
+		text: "The story takes place in the town of labyrinths, Orario, also called dungeon. Bell Cranel is a newbie adventurer that admires Having a fateful meeting with a person of the opposite gender. One day during an adventure, he is attacked by a Minotaur. Close to destruction, he is saved by a top-class female adventurer, Aizu Wallenstein. He falls in love with her at first sight, and begins actions to become an adventurer that is on her level. He later encounters Hestia, a god called a Lolita god due to her stature who falls in love with him."
+	},
+	{
+		url: "http://anilist.co/img/dir/anime/reg/20829-cDkQPmYZ6O0I.jpg",
+		name: "Owari no Seraph",
+		genre: "Drama, Shounen, Supernatural, Vampire",
+		episodes: "12",
+		duration: "24 min",
+		source: "Manga",
+		text: "The story takes place in a world where an unknown virus has killed the entire human population except for children under 13. Those children were then enslaved by vampires. The manga centers on Yuichiro Hyakuya, a human who dreams of becoming strong enough to kill all vampires."
+	},
+	{
+		url: "http://anilist.co/img/dir/anime/reg/20935-nMV41twhNq93.jpg",
+		name: "Arslan Senki",
+		genre: "Action, Adventure, Historical, Shounen",
+		episodes: "25",
+		duration: "24 min",
+		source: "Manga",
+		text: "The mangas story: In the prosperous kingdom of Pars lies the Royal Capital of Ecbatana, a city of splendor and wonder, ruled by the undefeated and fearsome King Andragoras. Arslan is the young and curious prince of Pars who, despite his best efforts, doesn't seem to have what it takes to be a proper king like his father."
+	},
+	{
+		url: "http://anilist.co/img/dir/anime/reg/20792-463frI6M1L7K.jpg",
+		name: "Fate/stay night: Unlimited Blade Works 2nd Season",
+		genre: "Action, Fantasy, Magic, Shounen, Supernatural",
+		episodes: "13",
+		duration: "24 min",
+		source: "Video Game",
+		text: "The 2nd season of ufotable's adaptation of the Unlimited Blade Works route of Fate/stay night."
+	},
+	{
+		url: "http://anilist.co/img/dir/anime/reg/21018-CTwGjayICk6e.jpg",
+		name: "Denpa Kyoushi",
+		genre: "Comedy, Harem, Romance, School, Shounen",
+		episodes: "24",
+		duration: "24 min",
+		source: "Manga",
+		text: "An anime adaptation of the manga which centers around Junichirou, a young man obssesed with manga, anime and games. He was a young genius who published papers in the journals Nature and Science when he was only 17. However, after graduating college, he became a NEET (Not in Education, Employment, or Training) absorbed in his anime blog. Unable to watch Kagami throw away his life, his sister Suzune gets him to work at the Ichou Academy she attends. The head of the school's board, Koyomi Hiiragi, assigns Kagami to his new life as a teacher."
+	},
+	{
+		url: "http://anilist.co/img/dir/anime/reg/20996-R7Nkq7Vtmfk0.jpg",
+		name: "Gintama 2015",
+		genre: "Action, Comedy, Historical, Parody, Sci-Fi, Shounen",
+		episodes: "?",
+		duration: "24 min",
+		source: "Manga",
+		text: "Gintoki, Shinpachi, Kagura and the rest of the Gintama group are back again for more fruitful adventures and hilarious banter!"
+	},
+	{
+		url: "http://anilist.co/img/dir/anime/reg/20745-c0OtttMUEE2r.jpg",
+		name: "High School DxD BorN",
+		genre: "Action, Comedy, Ecchi, Harem, Romance, School",
+		episodes: "12",
+		duration: "24 min",
+		source: "Light Novel",
+		text: "The third season of High School DxD."
+	},
+	{
+		url: "http://anilist.co/img/dir/anime/reg/20727-bWfzRH9aIz8x.jpg",
+		name: "Kekkai Sensen",
+		genre: "Action, Adventure, Fantasy, Shounen, Super Power, Supernatural",
+		episodes: "12",
+		duration: "24 min",
+		source: "Manga",
+		text: "A breach between Earth and the netherworlds has opened up over the city of New York, trapping New Yorkers and creatures from other dimensions in an impenetrable bubble. They've lived together for years, in a world of crazy crime sci-fi sensibilities. Now someone is threatening to sever the bubble, and a group of stylish superhumans is working to keep it from happening."
+	},
+	{
+		url: "http://anilist.co/img/dir/anime/reg/20876-eIgUEmjn97Nf.jpg",
+		name: "Nisekoi 2nd Season",
+		genre: "Comedy, Harem, Romance, School, Shounen",
+		episodes: "12",
+		duration: "24 min",
+		source: "Manga",
+		text: "Season 2 of Nisekoi."
+	},
+	{
+		url: "http://anilist.co/img/dir/anime/reg/20966-TYRotSnAtx5q.jpg",
+		name: "Yamada-kun to 7-nin no Majo",
+		genre: "Comedy, Mystery, School, Harem, Shounen, Supernatural",
+		episodes: "12",
+		duration: "24 min",
+		source: "Manga",
+		text: "Ryu Yamada is a second year student at Suzaku High. Ryu is always late for school, naps in class and gets abysmal grades. His life is a dead bore. The beautiful Urara Shiraishi, on the other hand, is Suzaku High's brightest student. One day, without explanation, their bodies are swapped! Ryu ends up in Urara's body, and Urara in Ryu's."
+	},
+	{
+		url: "http://anilist.co/img/dir/anime/reg/20839-BMkgNftmbbUQ.jpg",
+		name: "Triage X",
+		genre: "Action, Drama, Ecchi, Harem, School, Shounen",
+		episodes: "10",
+		duration: "23 min",
+		source: "Manga",
+		text: "Mochizuki General Hospital boasts some of the most well-trained (and well-endowed) nurses in town. But though these ladies spend much of their day battling disease, their after-hours are spent fighting a very different sort of sickness... Under the leadership of the hospital chairman, a handful of staff members and local teenagers form a group of mercenary assassins, targeting the cancers of society and excising those individuals before their wickedness spreads."
+	},
+	{
+		url: "http://anilist.co/img/dir/anime/reg/21058-zV6KknGdm8PH.jpg",
+		name: "Akagami no Shirayuki-hime",
+		genre: "Drama, Fantasy, Historical, Romance, Shoujo",
+		episodes: "12",
+		duration: "24 min",
+		source: "Manga",
+		text: "In the kingdom of Tanbarun lives Shirayuki, an independent and strong-willed young woman. Her resourceful intelligence has led her become a skilled pharmacist, but her most defining trait is her shock of beautiful apple-red hair. Her dazzling mane gets her noticed by the prince of the kingdom, but instead of romancing her, he demands she be his concubine. Shirayuki refuses, chops off her lovely locks, and runs away to the neighboring kingdom of Clarines. There, she befriends a young man named Zen, who, SURPRISE, is also a prince, although with a much better temperament than the previous one. Watch as Shirayuki finds her place in the new kingdom, and in Zen’s heart."
+	},
+	{
+		url: "http://anilist.co/img/dir/anime/reg/21175-LGTD4kHzdO7J.jpg",
+		name: "Dragon Ball Super",
+		genre: "Action, Adventure, Martial Arts, Shounen, Super Power",
+		episodes: "100",
+		duration: "24 min",
+		source: "Original",
+		text: "The first new Dragon Ball TV series story since Dragonball GT. The story is set a few years after the defeat of Majin Buu, when the Earth has become peaceful once again."
+	},
+	{
+		url: "http://anilist.co/img/dir/anime/reg/21055-C1YXQeUymnXF.jpg",
+		name: "Choas Dragon",
+		genre: "Action, Fantasy, Supernatural",
+		episodes: "12",
+		duration: "24 min",
+		source: "Original",
+		text: "The story takes place in the sparkling era year 3015, where the great nations Donatia and Kouran divide the world in a cold war as they fight for supremacy. In the middle of this standoff is island country Nil Kamui, which has lost its independence."
+	},
+	{
+		url: "http://anilist.co/img/dir/anime/reg/20773-FAjRQqK6OHk9.png",
+		name: "Gangsta",
+		genre: "Action, Drama, Seinen",
+		episodes: "12",
+		duration: "24 min",
+		source: "Manga",
+		text: "In the city of Ergastulum, a shady ville filled with made men and petty thieves, whores on the make and cops on the take, there are some deeds too dirty for even its jaded inhabitants to touch. Enter the Handymen, Nic and Worick, who take care of the jobs no one else will handle. Until the day when a cop they know on the force requests their help in taking down a new gang muscling in on the territory of a top Mafia family. It seems like business (and mayhem) as usual, but the Handymen are about to find that this job is a lot more than they bargained for."
+	},
+	{
+		url: "http://anilist.co/img/dir/anime/reg/20487-mKafHUJkr1bM.jpg",
+		name: "Gatchaman Crowds Insight",
+		genre: "Sci-Fi",
+		episodes: "12",
+		duration: "23 min",
+		source: "Original",
+		text: "2nd Season of Gatchman Crowds"
+	},
+	{
+		url: "http://anilist.co/img/dir/anime/reg/20849-JejsiwEwVsNZ.jpg",
+		name: "God Eater",
+		genre: "Action, Fantasy",
+		episodes: "13",
+		duration: "24 min",
+		source: "Video Game",
+		text: "The Far East, 2071. The domain of the mad gods. In the early 2050s, unknown life forms called Oracle cells begin their uncontrolled consumption of all life on Earth. Their ravenous appetite and remarkable adaptability earn them first dread, then awe, and finally the name aragami. In the face of an enemy completely immune to conventional weapons, urban civilization collapses, and each day humanity is driven further and further toward extinction. One single ray of hope remains for humanity. Following the development of God Arcs—living weapons which incorporate Oracle cells—their wielders are organized into an elite force. In a world ravaged by mad gods, these God Eaters fight a desperate war..."
+	},
+	{
+		url: "http://anilist.co/img/dir/anime/reg/21033-3HMPCgO0A62W.png",
+		name: "Jitsu wa Watashi wa",
+		genre: "Comedy, Fantasy, Romance, School, Shounen",
+		episodes: "13",
+		duration: "24 min",
+		source: "Manga",
+		text: "The romantic comedy centers around Asahi Kuromine, who has a crush on a cute girl named Yōko Shiragami. Shiragami just happens to be a vampire. Asahi cannot keep a secret, but he is determined to keep Shiragami's secret anyways."
+	},
+	{
+		url: "http://anilist.co/img/dir/anime/reg/20955-O5WXIusW8wfC.jpg",
+		name: "Rokka no Yuusha",
+		genre: "Action, Adventure, Fantasy, Mystery",
+		episodes: "12",
+		duration: "24 min",
+		source: "Manga",
+		text: "When the Majin awoke from the depths of darkness, the deity of fate chose six heroes and bequeathed them with the power to save the world. Adoretto, a boy who proclaims himself the strongest on Earth, was selected among the Rokka no Yuusha (Heroes of the Six Flowers), and he goes to the rendezvous point — but seven have gathered there. The heroes suspect that someone among the seven is the enemy, and the initial suspicion falls on Adoretto."
+	},
+	{
+		url: "http://anilist.co/img/dir/anime/reg/21038-hbRdHhHqZsxv.jpg",
+		name: "Ushio to Tora",
+		genre: "Shounen, Supernatural",
+		episodes: "26",
+		duration: "24 min",
+		source: "Manga",
+		text: "A new anime adaptation of the manga Ushio to Tora."
+	},
+	{
+		url: "http://anilist.co/img/dir/anime/reg/20785-GbXysHDJPSzA.jpg",
+		name: "Absolute Duo",
+		genre: "Action, Ecchi, Harem, Romance, School, Supernatural",
+		episodes: "12",
+		duration: "24 min",
+		source: "Light Novel",
+		text: "The story begins with Blaze. A weapon that is the manifestation of a human's soul. Tooru Kokonoe happens to be qualified for this, but for some reason his Blaze is not a weapon, but a shield. Moreover, he enrolled in a school that teaches combat skills, and thanks to the school's Duo partner system, he ends up living with a beautiful silver haired girl"
+	},
+	{
+		url: "http://anilist.co/img/dir/anime/reg/20755-dd7F1pv6z1y9.jpg",
+		name: "Ansatsu Kyoushitsu",
+		genre: "Action, Comedy, School, Shounen, Supernatural",
+		episodes: "22",
+		duration: "23 min",
+		source: "Manga",
+		text: "The students of class 3-E have a mission: kill their teacher before graduation. He has already destroyed the moon, and has promised to destroy the Earth if he can not be killed within a year. But how can this class of misfits kill a tentacled monster, capable of reaching Mach 20 speed, who may be the best teacher any of them have ever had?"
+	},
+	{
+		url: "http://anilist.co/img/dir/anime/reg/20931-AWa51FoEzSs9.jpg",
+		name: "Death Parade",
+		genre: "Action, Psychological",
+		episodes: "12",
+		duration: "24 min",
+		source: "Original",
+		text: "Welcome to Quindecim What greets two unsuspecting guests is a strange bar, Quindecim, and the white-haired bartender, Decim. From here you two shall begin a battle where your lives hang in the balance, he says to introduce the Death Game. Before long the guests true natures become apparent. As a matter of course, at the game's end Decim is revealed to be the arbiter. Decim's judgement on the two guests is..."
+	},
+	{
+		url: "http://anilist.co/img/dir/anime/reg/20910-nFhipmAVBvP5.jpg",
+		name: "Shimoneta to Iu Gainen ga Sonzai Shinai Taikutsu na Sekai",
+		genre: "Comedy, Ecchi, School",
+		episodes: "12",
+		duration: "23 min",
+		source: "Light Novel",
+		text: "In the city of Ergastulum, a shady ville filled with made men and petty thieves, whores on the make and cops on the take, there are some deeds too dirty for even its jaded inhabitants to touch. Enter the Handymen, Nic and Worick, who take care of the jobs no one else will handle. Until the day when a cop they know on the force requests their help in taking down a new gang muscling in on the territory of a top Mafia family. It seems like business (and mayhem) as usual, but the Handymen are about to find that this job is a lot more than they bargained for."
+	},
+	{
+		url: "http://anilist.co/img/dir/anime/reg/20758.jpg",
+		name: "Juuou Mujin no Fafnir",
+		genre: "Ecchi, Fantasy, Harem, Romance, School",
+		episodes: "12",
+		duration: "24 min",
+		source: "Manga",
+		text: "In the city of Ergastulum, a shady ville filled with made men and petty thieves, whores on the make and cops on the take, there are some deeds too dirty for even its jaded inhabitants to touch. Enter the Handymen, Nic and Worick, who take care of the jobs no one else will handle. Until the day when a cop they know on the force requests their help in taking down a new gang muscling in on the territory of a top Mafia family. It seems like business (and mayhem) as usual, but the Handymen are about to find that this job is a lot more than they bargained for."
+	},
+	{
+		url: "http://anilist.co/img/dir/anime/reg/20725-Vm6ZzEFvT6w3.jpg",
+		name: "Kuroko no Basuke 3rd Season",
+		genre: "Comedy, School, Shounen, Sports",
+		episodes: "12",
+		duration: "24 min",
+		source: "Manga",
+		text: "In the city of Ergastulum, a shady ville filled with made men and petty thieves, whores on the make and cops on the take, there are some deeds too dirty for even its jaded inhabitants to touch. Enter the Handymen, Nic and Worick, who take care of the jobs no one else will handle. Until the day when a cop they know on the force requests their help in taking down a new gang muscling in on the territory of a top Mafia family. It seems like business (and mayhem) as usual, but the Handymen are about to find that this job is a lot more than they bargained for."
+	},
+	{
+		url: "http://anilist.co/img/dir/anime/reg/20850-rgprgcyacc6P.jpg",
+		name: "Tokyo Ghoul 2nd Season",
+		genre: "Horror, Mystery, Psychological, Seinen, Supernatural",
+		episodes: "12",
+		duration: "24 min",
+		source: "Manga",
+		text: "In the city of Ergastulum, a shady ville filled with made men and petty thieves, whores on the make and cops on the take, there are some deeds too dirty for even its jaded inhabitants to touch. Enter the Handymen, Nic and Worick, who take care of the jobs no one else will handle. Until the day when a cop they know on the force requests their help in taking down a new gang muscling in on the territory of a top Mafia family. It seems like business (and mayhem) as usual, but the Handymen are about to find that this job is a lot more than they bargained for."
+	},
+]
